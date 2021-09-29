7b27109a9cce585e05d7003193dc4b58b4385097
alenjb <alenjb@naver.com>
2021-09-29 10:36:00 +0900

    Prob 4 완성

--- BaekJoon/src/Stage7/Prob4.java
+++ BaekJoon/src/Stage7/Prob4.java
@@ -3,22 +3,25 @@ package Stage7;
 	import java.io.BufferedReader;
 	import java.io.InputStreamReader;
 
+
 	public class Prob4 {
 		public static void main (String[] args)throws Exception {
 			
 			BufferedReader bf= new BufferedReader(new InputStreamReader(System.in));
 			int ch= Integer.parseInt(bf.readLine());
-			System.out.println("");
 			for(int i=0; i<ch; i++) {
 				String []ss= bf.readLine().split(" ");
 				int num= Integer.parseInt(ss[0]);
-				String s1=ss[1];
+				String []str=ss[1].split("");
 
-				for(int k=0; k<s1.length(); k++) {
+				for(int k=0; k<str.length; k++) {
 					for(int j=0; j<num; j++) {
-						System.out.print(s1.charAt(k));
+						System.out.print(str[k]);
 					}
+
 				}
+				System.out.println();
+
 						
 				
 			}
