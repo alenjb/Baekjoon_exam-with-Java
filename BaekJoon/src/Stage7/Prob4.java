80ff2c4700500f96cae15532250e8aa3cfea8e9e
alenjb <alenjb@naver.com>
2021-09-29 10:03:45 +0900

    prob 4 출력오류 있는 ver

--- BaekJoon/src/Stage7/Prob4.java
+++ BaekJoon/src/Stage7/Prob4.java
@@ -1,7 +1,30 @@
 package Stage7;
 
-public class Prob4 {
-	public static void main(String[] args) {
-		int a;
+	import java.io.BufferedReader;
+	import java.io.InputStreamReader;
+
+	public class Prob4 {
+		public static void main (String[] args)throws Exception {
+			
+			BufferedReader bf= new BufferedReader(new InputStreamReader(System.in));
+			int ch= Integer.parseInt(bf.readLine());
+			System.out.println("");
+			for(int i=0; i<ch; i++) {
+				String []ss= bf.readLine().split(" ");
+				int num= Integer.parseInt(ss[0]);
+				String s1=ss[1];
+
+				for(int k=0; k<s1.length(); k++) {
+					for(int j=0; j<num; j++) {
+						System.out.print(s1.charAt(k));
+					}
+				}
+						
+				
+			}
+			
+
+		}
 	}
-}
+
+
