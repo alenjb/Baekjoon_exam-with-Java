c8e2598783e961a0e8d7b2d46ba4cfc5ed09f788
alenjb <alenjb@naver.com>
2021-10-02 21:39:07 +0900

    변화 이전 파일

--- BaekJoon/src/Stage7/Prob10.java
+++ BaekJoon/src/Stage7/Prob10.java
@@ -15,25 +15,32 @@ public class Prob10 {
 		
 		for(int i=0; i<ch;i++) {//한줄씩 읽기
 			strs[i]= bf.readLine();
-			
+			count1=0;
 			for(int j=0; j<strs[i].length(); j++) {//첫글자부터
-				count1=0;
+				
 				for(int k=j+1; k<strs[i].length(); k++) { //한글자 한글자 검색
 					if (Character.toString(strs[i].charAt(k)).equals(Character.toString(strs[i].charAt(j)))){
-						System.out.println(Character.toString(strs[i].charAt(k))+"와"+Character.toString(strs[i].charAt(j)));
-						System.out.println(k);
-						int a=k, b=1;//a:다음 글자  b:연속된거 몇갠지
-						while(a<strs[i].length() &&Character.toString(strs[i].charAt(a)).equals(Character.toString(strs[i].charAt(j)))
+		
+//						System.out.println(Character.toString(strs[i].charAt(k))+"와"+Character.toString(strs[i].charAt(j)));
+//						System.out.println(k);
+						int a=k+1, b=1;//a:다음 글자  b:연속된거 몇갠지
+						while(a<strs[i].length() &&Character.toString(strs[i].charAt(a)).contentEquals(Character.toString(strs[i].charAt(k)))
 								) {
+							System.out.println("동과");
 							b++; a++;
 						}
 						int len= strs[i].length();
-						int len2= strs[i].replace(Character.toString(strs[i].charAt(j)), "").length();
+						int len2= strs[i].replace(Character.toString(strs[i].charAt(k)), "").length();
+						System.out.println("j   k  a  "+j+ "  "+k+"  "+a);
+						System.out.println(Character.toString(strs[i].charAt(a)) +" "+Character.toString(strs[i].charAt(k)));
+						System.out.println(Character.toString(strs[i].charAt(a)).contentEquals(Character.toString(strs[i].charAt(k))));
+						System.out.println("len, len2  b "+len+" "+len2+" "+b);
+						System.out.println("");
 						if (len2+b!=len) count1++; 
 					}
-				}
-				}
-			if( count1==0) count++;
+				} //System.out.print("  "+j+"일 떼  "+count1);
+				}if( count1==0) count++;
+			
 			}// for i
 		System.out.println(count);
 		}
