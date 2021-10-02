285126aedb66f4070d0c5a9fe3f9fc59702abee1
alenjb <alenjb@naver.com>
2021-10-02 19:08:06 +0900

    add Prob5

--- BaekJoon/src/Stage7/Prob10.java
+++ BaekJoon/src/Stage7/Prob10.java
@@ -8,22 +8,36 @@ public class Prob10 {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		int ch= Integer.parseInt(bf.readLine());
 		String [] strs= new String [ch];
+		//결과 값
 		int count=0;
+		//임시 값: count1이 0 이상이면 그룹단어가 아님
 		int count1=0;
-		for(int i=0; i<ch;i++) {
+		
+		for(int i=0; i<ch;i++) {//한줄씩 읽기
 			strs[i]= bf.readLine();
-			for(int j=0; j<strs[i].length(); j++) {
-				int length= strs[i].length();
+			
+			for(int j=0; j<strs[i].length(); j++) {//첫글자부터
 				count1=0;
-				if((strs[i].replace(Character.toString(strs[i].charAt(j)),"").length()!= length-1) &&
-						!strs[i].contains(Character.toString(strs[i].charAt(j))+Character.toString(strs[i].charAt(j)))) {
-					count1++;
+				for(int k=j+1; k<strs[i].length(); k++) { //한글자 한글자 검색
+					if (Character.toString(strs[i].charAt(k)).equals(Character.toString(strs[i].charAt(j)))){
+						System.out.println(Character.toString(strs[i].charAt(k))+"와"+Character.toString(strs[i].charAt(j)));
+						System.out.println(k);
+						int a=k, b=1;//a:다음 글자  b:연속된거 몇갠지
+						while(a<strs[i].length() &&Character.toString(strs[i].charAt(a)).equals(Character.toString(strs[i].charAt(j)))
+								) {
+							b++; a++;
+						}
+						int len= strs[i].length();
+						int len2= strs[i].replace(Character.toString(strs[i].charAt(j)), "").length();
+						if (len2+b!=len) count1++; 
+					}
 				}
-				
-			}if( count1==0) count++;
-		}
+				}
+			if( count1==0) count++;
+			}// for i
 		System.out.println(count);
-		System.out.println(!strs[0].contains(Character.toString(strs[0].charAt(0))+Character.toString(strs[0].charAt(0))));
+		}
 		
 	}
-}
+
+/*처음 나올 떄 발견 -> 그 때부터 연속된거 다 ""로 바꾸기 -> 포함되어 있으면 count1++*/
