17d4491aa44cc69519a6dedf7821a5be7ab0c110
alenjb <alenjb@naver.com>
2021-10-15 18:54:44 +0900

    solve Prob4

---
+++ BaekJoon/src/Stage8/Prob6.java
@@ -0,0 +1,13 @@
+package Stage8;
+
+public class Prob6 {
+	public static void main(String[] args) {
+		
+	}
+	public static int cal(int k, int n, int result) {
+		if(k==0)  {result= result+n*(n+1)/2; return result;}
+		else {
+			
+		}
+	}
+}
