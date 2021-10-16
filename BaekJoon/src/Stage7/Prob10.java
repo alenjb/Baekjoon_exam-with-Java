d38f6f78aae2555d27193ab7ddf41819fa1a89de
alenjb <alenjb@naver.com>
2021-10-16 22:16:22 +0900

    필요없는 부분 제거하고 조건문 충족시키면 바로 break 하도록 추가

--- BaekJoon/src/Stage7/Prob10.java
+++ BaekJoon/src/Stage7/Prob10.java
@@ -8,48 +8,41 @@ public class Prob10 {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		int ch= Integer.parseInt(bf.readLine());
 		String [] strs= new String [ch];
-		//결과 값
-		int count=0;
-		//임시 값: count1이 0 이상이면 그룹단어가 아님
-		int count1=0;
+		
+		int count=0; //결과 값
+		int count1=0;//임시 값: count1이 0 이상이면 그룹단어가 아님
 		
 		for(int i=0; i<ch;i++) {//한줄씩 읽기
 			strs[i]= bf.readLine();
-			count1=0;
+			count1=0;//임시값을 0으로 설정
 			for(int j=0, k=j+1; j<strs[i].length()-1; j++) {//첫글자부터
 				k=j+1;
-				if(Character.toString(strs[i].charAt(k)).equals(Character.toString(strs[i].charAt(j)))){
-//					System.out.println("k    j   "+strs[i].charAt(k)+" "+(strs[i].charAt(j)));
-//					System.out.println(k);
-					while(k<strs[i].length()&& Character.toString(strs[i].charAt(k)).equals(Character.toString(strs[i].charAt(j)))) {
-//						System.out.println("아"+k);
-						
+				if(Character.toString(strs[i].charAt(k)).equals(Character.toString(strs[i].charAt(j)))){//옆글자와 같으면
+					while(k<strs[i].length()&& Character.toString(strs[i].charAt(k)).equals(Character.toString(strs[i].charAt(j)))) { //연속된 글자가 같을 때까지 k 증가
 						k++; 
 					}
-					int b=k-j;
-					int len=strs[i].length();
-					int len2= strs[i].replace(Character.toString(strs[i].charAt(j)), "").length();
-//					System.out.println(i+"번쨰의"+ j+ "번쨰"+len2+"  "+ "b  "+  b);
-					if (len- len2!=b) {
-						count1++; //System.out.println("으악");
-					}j=j+b;
+					int successive=k-j; //연속된 수
+					int len=strs[i].length(); //길이
+					int len2= strs[i].replace(Character.toString(strs[i].charAt(j)), "").length(); //연속된 글자를 없앴을 때의 길이
+					if (len- len2!=successive) {count1++; break;} //연속된거 외에 같은 값이 있으면 임시값 증가
+					j=k;//k-1까지는 연속된 값이므로 k부터 검사
 					}
-				else {
-					int len=strs[i].length();
-					int len2= strs[i].replace(Character.toString(strs[i].charAt(j)), "").length();
-					if (j-1>=0&& !Character.toString(strs[i].charAt(j-1)).equals(Character.toString(strs[i].charAt(j))) &&len- len2!=1) {count1++; /*System.out.println(i+" 으악 2"+ "  "+j);*/
-					
+				else {//옆글자와 같지 않으면
+					int len=strs[i].length(); //길이
+					int len2= strs[i].replace(Character.toString(strs[i].charAt(j)), "").length(); //현재글자와 같은 글자를 없앴을 때의 길이
+					if (len- len2!=1){// 어딘가에 같은 값이 있을 때
+					if (j>=1&& j<len-1&& !Character.toString(strs[i].charAt(j-1)).equals(Character.toString(strs[i].charAt(j))) &&len- len2!=1){// 그 값이 전의 글자와 다르면
+						count1++;}
 					}
-				}} //System.out.print("  "+j+"일 떼  "+count1);
-			int j=strs[i].length()-1;
-			int len=strs[i].length();
-			int len2= strs[i].replace(Character.toString(strs[i].charAt(j)), "").length();
-			if (j-1>=0&& !Character.toString(strs[i].charAt(j-1)).equals(Character.toString(strs[i].charAt(j))) &&len- len2!=1) {count1++; ;
+				}} 
 			
-			}
+//			int j=strs[i].length()-1;
+//			int len=strs[i].length();
+//			int len2= strs[i].replace(Character.toString(strs[i].charAt(j)), "").length();
+//			if (j-1>=0&& !Character.toString(strs[i].charAt(j-1)).equals(Character.toString(strs[i].charAt(j))) &&len- len2!=1) count1++;
 			if (count1==0) count++;
 			}// for i
-		System.out.println(count);
+		System.out.print(count);
 	}
 	
 }
