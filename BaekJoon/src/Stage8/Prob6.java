fe03d52a344d71bdaa24291f4727364a2ee0dfa1
alenjb <alenjb@naver.com>
2021-10-31 19:33:18 +0900

    update Prob6

--- BaekJoon/src/Stage8/Prob6.java
+++ BaekJoon/src/Stage8/Prob6.java
@@ -1,38 +1,34 @@
 package Stage8;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 
 public class Prob6 {
 	public static void main(String[] args) throws Exception{
 		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-		int k= Integer.parseInt(br.readLine());
-		int n= Integer.parseInt(br.readLine());
-		int result=0;
-		for(int i=n; i>0; i--) {
-			result=result+cal(k, i, result);
-			System.out.println(k +"   "+i+"  끝");
+		BufferedWriter bw= new BufferedWriter(new OutputStreamWriter(System.out));
+		int testCase= Integer.parseInt(br.readLine());
+		for(int a=0; a<testCase; a++) {
+			int k=Integer.parseInt(br.readLine());
+			int n=Integer.parseInt(br.readLine());
+			bw.write(check(k, n)+"\n");
 		}
-//		int a=cal(k,n, result);
-		System.out.println(result);
+		bw.close();
 	}
-	public static int cal(int height, int side, int result) {
-		if(height==0)  {
-			System.out.println(height+"   "+side);
-			result=result+side;
-			System.out.println("여기는 끝"+result+" height: "+height+" side: "+side);
-
-			if(side!=1) cal(height, side-1, result);
-			else {System.out.println("종료   "+result); return result; }
-			}
+	//인원을 계산하는 check함수
+	static int check(int k, int n) {
+		int result=0;
+		if(k==0)result=n;
 		else {
-				if(height-1>=0) {
-					System.out.println(height+ "  "+side);
-					result=result+cal(height-1, side, result);
-					System.out.println(result);
-					return result;
-					}
-				}
+			for(int i=1; i<=n; i++) {
+				//아래층 1호부터 n호까지 더하기
+				result=result+check(k-1, i);
+			}
+		}
 		return result;
+		
 	}
 }
