c741278a9f00ed014c432edf929e309d0e981e1a
alenjb <alenjb@naver.com>
2021-11-18 14:26:23 +0900

    Solve Prob3 using StringBuilder

--- BaekJoon/src/Stage10/Prob3_2.java
+++ BaekJoon/src/Stage10/Prob3_2.java
@@ -1,26 +1,65 @@
 package Stage10;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Prob3_2 {
-	public static void main(String[] args) {
-		//3으로 나눈 몫
-		int f=3;
-		String [][] arr= new String [27][27];
-		for(int i=0; i<arr[0].length; i++) {
-			for(int j=0; j<arr[0].length; j++) {
-				arr[i][j]="*";
+	static char[][] arr;
+ 
+	public static void main(String[] args) throws Exception{
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+ 
+		arr = new char[N][N];
+        
+		star(0, 0, N, false);
+ 
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				sb.append(arr[i][j]);
+			}
+			sb.append('\n');
+		}
+		System.out.print(sb);
+	}
+ 
+	static void star(int x, int y, int N, boolean blank) {
+ 
+		// 공백칸일 경우
+		if (blank) {
+			for (int i = x; i < x + N; i++) {
+				for (int j = y; j < y + N; j++) {
+					arr[i][j] = ' ';
+				}
+			}
+			return;
+		}
+ 
+		// 더이상 쪼갤 수 없는 블록일 때
+		if (N == 1) {
+			arr[x][y] = '*';
+			return;
+		}
+ 
+		/*
+		   N=27 일 경우 한 블록의 사이즈는 9이고, 
+		   N=9 일 경우 한 블록의 사이즈는 3이듯
+		   해당 블록의 한 칸을 담을 변수를 의미 size
+           
+		   count는 별 출력 누적을 의미
+		 */
+ 
+		int size = N / 3;
+		int count = 0;
+		for (int i = x; i < x + N; i += size) {
+			for (int j = y; j < y + N; j += size) {
+				count++;
+				if (count == 5) { // 공백 칸일 경우
+					star(i, j, size, true);
+				} else {
+					star(i, j, size, false);
+				}
 			}
 		}
-		int arr_length=arr[0].length;
-		for(int i=3^(f-1);i<arr_length ; i=i+3^(f-1)+1) {
-//			for(int i=3^(f-1);i<arr_length ; i=i+3^(f-1)+1) {
-			for(int j=3^(f-1);j<arr_length ; j=j+3^(f-1)+1) {
-
-			arr[i][j]=" ";}	}
-		for(int i=0; i<arr[0].length; i++) {
-			for(int j=0; j<arr[0].length; j++) {
-				System.out.print(arr[i][j]);
-			}
-			System.out.println();
-		}
-
-}}
+	}
+}
