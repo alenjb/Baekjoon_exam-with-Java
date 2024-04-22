aa9eb30313f873f479551a7589e41951c6e8b741
alenjb <alenjb@naver.com>
2024-04-22 11:14:24 +0900

    solve 영화감독_숌(1436)

---
+++ BaekJoon/src/영화감독_숌.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class 영화감독_숌 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 0;
+        for(int i =666;;i++){
+            String s = i+"";
+            if(s.contains("666")){
+                count++;
+                if(count == n){
+                    System.out.println(s);
+                    break;
+                }
+            }
+        }
+    }
+}
