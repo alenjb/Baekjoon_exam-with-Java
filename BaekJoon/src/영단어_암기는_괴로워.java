b97fd3c98d42dec1eb4ff641ec3291c309c0b4a9
alenjb <alenjb@naver.com>
2024-04-24 09:53:42 +0900

    solve 영단어_암기는_괴로워(20920):
    시간 단축 버전

--- BaekJoon/src/영단어_암기는_괴로워.java
+++ BaekJoon/src/영단어_암기는_괴로워.java
@@ -3,56 +3,48 @@ import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class 영단어_암기는_괴로워 {
-    static int M;
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        PriorityQueue<Word> pq = new PriorityQueue<>();
         ArrayList<String> arr = new ArrayList<>();
         int N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
         for(int i=0; i<N; i++){
             String now = br.readLine();
-            if(now.length() >= M){
-                arr.add(now);
-            }
+            if(now.length()<M) continue;
+            arr.add(now);
         }
-        ArrayList<String> arr2 = new ArrayList<>(arr);
-        for (String s : arr2) {
-            if(arr.contains(s)){
-                int frequency = Collections.frequency(arr, s);
-                arr.removeIf(a -> a.equals(s));
-                pq.add(new Word(s, frequency));
-            }
+        Map<String, Integer> map = new HashMap<>();
+        //빈도수 같이 저장
+        for (String s : arr) {
+            if(map.containsKey(s)) map.put(s, map.get(s)+1);
+            else map.put(s,1);
         }
-        while(!pq.isEmpty()){
-            Word poll = pq.poll();
-            bw.write(poll.word);
+
+        List<String> words = map.keySet().stream().collect(Collectors.toList());
+
+        words.sort((o1, o2) -> {
+            int v1 = map.get(o1);
+            int v2 = map.get(o2);
+
+            if(v1 == v2){
+                if(o1.length() == o2.length()){
+                    return o1.compareTo(o2);
+                }
+                return o2.length() - o1.length();
+            }
+            return v2-v1;
+        });
+
+        for (String word : words) {
+            bw.write(word);
             bw.newLine();
         }
         bw.flush();
 
-    }
-}
-class Word implements Comparable<Word>{
-    String word;
-    int count;
-    public Word(String word,int count){
-        this.word = word;
-        this.count = count;
-    }
-    public int compareTo(Word word){
-        if(word.count!= this.count) return word.count - this.count;
-        else if (word.word.length()!= this.word.length()) return word.word.length() - this.word.length();
-        else {
-            for(int i=0; i<word.word.length(); i++)
-                if(word.word.charAt(i) != this.word.charAt(i)){
-                    return this.word.charAt(i) - word.word.charAt(i);
-                }
-        }
-        return 0;
     }
 }
