b5a7f67bd0432660ab2a2f2cc53387781580b397
alenjb <alenjb@naver.com>
2024-05-08 23:50:25 +0900

    solve 나무_자르기(2805)

---
+++ BaekJoon/src/이분탐색/나무_자르기.java
@@ -0,0 +1,59 @@
+package 이분탐색;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class 나무_자르기 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long N = Long.parseLong(st.nextToken());
+        long M = Long.parseLong(st.nextToken());
+
+        long [] arr = new long[(int) N];
+
+        st = new StringTokenizer(br.readLine());
+        long max = 0;
+        for(int i=0; i<N; i++){
+            long num = Long.parseLong(st.nextToken());
+            arr[i] = num;
+            if(max < num) max = num;
+        }
+
+        // 이분 탐색
+        long left = 0;
+        long right = max+1;
+        long mid;
+        long answer = 0;
+        long scount=0;
+        while(left<right){
+            mid = (left + right) /2;
+
+            long count = 0;
+            for(int i=0; i<N; i++){
+                count = count + arr[i] - mid;
+            }
+            // 절단 가능
+            if(count >= M){
+                left = mid+1;
+                answer = mid;
+                scount = count;
+            }
+
+            //절단 실패
+            else {
+                right = mid;
+            }
+        }
+
+        System.out.println(answer);
+        System.out.println("scount = " + scount);
+
+
+    }
+}
